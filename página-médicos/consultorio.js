82deb80ad14e3208348af1962c1523e1578c4fda
Gustavo Andrade <gustavo20martinss@gmail.com>
2024-05-20 21:10:10 -0300

    adicionei o javascript no código da tela de médicos, nunca me estressei tanto

---
+++ página-médicos/consultorio.js
@@ -0,0 +1,34 @@
+const buttons = document.querySelectorAll('.button');
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const buttonId = button.id;
+    const block = document.getElementById(buttonId.replace('button-', ''));
+    const ocult = document.getElementById(buttonId.replace('button-', 'ocult-'));
+    const lista = document.querySelectorAll(`#${buttonId.replace('button-', 'lista-')} li`);
+
+    if (block) {
+      const isExpanded = button.dataset.expanded === 'true';
+      ocult.style.display =isExpanded ? "none" : "block";
+      block.style.height = isExpanded ? "350px" : "420px";
+      block.style.backgroundColor = isExpanded ? "#ffffff" : "#3B6978";
+      button.textContent = isExpanded ? "Mais Informações" : "Menos Informações";
+      button.style.color = isExpanded ? "#3b6978" : "#ffffff";
+      button.style.marginTop = isExpanded ? "32%" : " 38%"
+
+      lista.forEach(lista => {
+        lista.style.color = isExpanded ? "#204051" : "#ffffff";
+      });
+
+      button.dataset.expanded = isExpanded ? 'false' : 'true';
+    } else {
+      console.error("Error: Block or list element not found.");
+    }
+  });
+});
+
+
+  
+
+  
